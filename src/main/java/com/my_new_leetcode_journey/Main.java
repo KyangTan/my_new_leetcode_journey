780f564c238a9ba36d5901dc117f890253f11bf4
KyangTan <kwanyang29@gmail.com>
2024-10-23 22:32:05 +0800

    feat: completed MaxWaterContainer

--- src/main/java/com/my_new_leetcode_journey/Main.java
+++ src/main/java/com/my_new_leetcode_journey/Main.java
@@ -1,16 +1,16 @@
 package com.my_new_leetcode_journey;
 
-import java.util.List;
-
-import com.my_new_leetcode_journey.arrays_hashing.LongestConsecutiveSequence;
-import com.my_new_leetcode_journey.two_pointers.ThreeIntegerSum;
-import com.my_new_leetcode_journey.two_pointers.TwoIntegerSumII;
+import com.my_new_leetcode_journey.two_pointers.MaxWaterContainer;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4,-2,-3,3,0,4};
+        int[] nums = {1,7,2,5,4,7,3,6};
 
-        List<List<Integer>> answer = ThreeIntegerSum.threeSumSelfAttempt(nums);
+        int answer = MaxWaterContainer.maxAreaSelfAttempt(nums);
+
+        System.out.println(answer);
+
+        // List<List<Integer>> answer = ThreeIntegerSum.threeSumSelfAttempt(nums);
 
         // System.out.print("answer: [");
         // for (int i : answer) {
---
+++ src/main/java/com/my_new_leetcode_journey/two_pointers/MaxWaterContainer.java
@@ -0,0 +1,84 @@
+package com.my_new_leetcode_journey.two_pointers;
+
+/**
+ * <h1>Max Water Container</h1>
+ * <h2>Medium</h2>
+ * <p>
+ * You are given an integer array <code>heights</code> where
+ * <code>heights[i]</code> represents the height of the
+ * <code>i</code><sup>th</sup> bar.
+ * </p>
+ * <p>
+ * You may choose any two bars to form a container. Return the maximum amount of
+ * water a container can store.
+ * </p>
+ * 
+ * <h3>Example 1:</h3>
+ * 
+ * <pre>
+ * Input: height = [1, 7, 2, 5, 4, 7, 3, 6]
+ * Output: 36
+ * </pre>
+ * 
+ * <h3>Example 2:</h3>
+ * 
+ * <pre>
+ * Input: height = [2, 2, 2]
+ * Output: 4
+ * </pre>
+ * 
+ * <h3>Constraints:</h3>
+ * <ul>
+ * <li>2 <= height.length <= 1000</li>
+ * <li>0 <= height[i] <= 1000</li>
+ * </ul>
+ */
+public class MaxWaterContainer {
+
+    /**
+     * <h3>Time & Space Complexity:</h3>
+     * 
+     * <p>
+     * <b>Time Complexity:</b> O(n)
+     * </p>
+     * <ul>
+     * <li>Where <code>n</code> is the length of the input.</li>
+     * </ul>
+     * 
+     * <p>
+     * <b>Space Complexity:</b> O(1)
+     * </p>
+     * <ul>
+     * <li>Constant space usage, independent of input size.</li>
+     * </ul>
+     */
+    public static int maxAreaSelfAttempt(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int max = 0;
+        while (left < right) {
+            if (heights[left] > heights[right]) {
+                int temp = heights[right] * (right - left);
+                if (temp > max) {
+                    max = temp;
+                }
+                right--;
+            }
+            if (heights[right] > heights[left]) {
+                int temp = heights[left] * (right - left);
+                if (temp > max) {
+                    max = temp;
+                }
+                left++;
+            }
+            if (heights[right] == heights[left]) {
+                int temp = heights[left] * (right - left);
+                if (temp > max) {
+                    max = temp;
+                }
+                left++;
+            }
+        }
+        return max;
+    }
+}
