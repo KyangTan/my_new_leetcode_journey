0b503bf0be7806f11d733ecbd27c330639fb2e82
KyangTan <kwanyang29@gmail.com>
2024-10-28 12:07:55 +0800

    completed CarFleet

---
+++ src/main/java/com/my_new_leetcode_journey/stack/CarFleet.java
@@ -0,0 +1,30 @@
+package com.my_new_leetcode_journey.stack;
+
+import java.util.Arrays;
+
+public class CarFleet {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] pairs = new double[n][2];
+
+        for(int i = 0; i< n; i++){
+            pairs[i][0] = position[i];
+            pairs[i][1] = speed[i];
+        }
+
+        Arrays.sort(pairs, (a,b)-> Double.compare(b[0], a[0]));
+
+        int fleetCount = 0;
+        double[] timeToReach = new double[n];
+
+        for(int i =0; i< n; i++){
+            timeToReach[i] = (target - pairs[i][0])/pairs[i][1];
+            if(i>= 1 && timeToReach[i] <= timeToReach[i-1]){
+                timeToReach[i] = timeToReach[i-1];
+            }else{
+                fleetCount++;
+            }
+        }
+        return fleetCount;
+    }
+}
